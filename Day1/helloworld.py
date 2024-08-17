52d0eeefe57f863561f1ec235530cacae149fcff
Eliezer Nsengi <avaranstudios@gmail.com>
2024-08-17 12:29:07 +0200

    add: day one

--- Day1/helloworld.py
+++ Day1/helloworld.py
@@ -1,1 +1,18 @@
 print("Hello world!")
+print( 2 + 3) # addition
+print( 2 * 3)  # multiplication
+print(3 /2 )  # division
+print( 3 **2 ) #exponentiation
+print(3%2) # modulus
+
+
+
+print(type(10)) #Int
+print(type(3.14)) #Float
+print(1 + 3j) #complex
+print("iAmNsengi") #String
+print([1,2,3,4,5]) #List
+print({'name':"i am Nsengi"}) #Dictionary
+print(type({1.2,3,2.4}))  #Set
+print(type((9.8,3.14,2.7))) #Tuple
+
